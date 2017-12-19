9d4f66ee4f7c0c7e52b109cce3b8e2288613245e
root <root@debian>
2017-12-19 15:34:55 +0100

    modified:   src/counting.c

--- thesis_test/thesis_test/src/counting.c
+++ thesis_test/thesis_test/src/counting.c
@@ -12,57 +12,63 @@ float moving_hysteresis(float Delta,movAverage *temp)
     float result;
 
     if(temp->measuredValue<=temp->act_max_value)
+    {
+        if(temp->measuredValue >= temp->act_min_val)
         {
-            if(temp->measuredValue >= temp->act_min_val)
-                {
-                    result=temp->act_min_val;
-                    return result;
-                }
-            else
-                {
-                    temp->act_min_val=temp->measuredValue;
-                    temp->act_max_value=temp->act_min_val+Delta;
-                    result=temp->act_min_val;
-                    return result;
-                }
-        }
-    else
-        {
-            temp->act_max_value=temp->measuredValue;
-            temp->act_min_val=temp->act_max_value-Delta;
             result=temp->act_min_val;
-            return  result;
+            return result;
         }
+        else
+        {
+            temp->act_min_val=temp->measuredValue;
+            temp->act_max_value=temp->act_min_val+Delta;
+            result=temp->act_min_val;
+            return result;
+        }
+    }
+    else
+    {
+        temp->act_max_value=temp->measuredValue;
+        temp->act_min_val=temp->act_max_value-Delta;
+        result=temp->act_min_val;
+        return  result;
+    }
 }
 /**
 Moving average with 3 tagNumber in default
 */
 float mov_average(movAverage *temp,Threadcommon *Tag,int address)
 {
-    float result=0.0;
-    float sum;
-    int i,j;
+    float result,sum;
+    result=sum=0.0;
+    int i,j,k;
     int tagNumber;
     tagNumber=Tag->sensors[address].movAve_tag_number;
-
+    k=tagNumber-1;
     /**tagNumber are odd */
     if(tagNumber%2)
+    {
+        for(i=0; i<tagNumber; i++)
+            sum+=temp->k_element[i];
+        for(j=0; j<tagNumber-1; j++)
         {
-            for(i=0; i<tagNumber; i++)
-                sum+=temp->k_element[i];
-            for(j=0; j<tagNumber-1; j++)
-                temp->k_element[j+1]=temp->k_element[j];
-            result=sum/(float)tagNumber;
+            temp->k_element[k]=temp->k_element[k-1];
+            k--;
         }
+        result=sum/(float)tagNumber;
+    }
     else
+    {
+        for(i=0; i<tagNumber; i++)
+            sum+=(temp->k_element[i]);
+        for(j=0; j<tagNumber-1; j++)
         {
-            for(i=0; i<tagNumber; i++)
-                sum+=(temp->k_element[i]);
-            for(j=0; j<tagNumber-1; j++)
-                temp->k_element[j+1]=temp->k_element[j];
-            sum=sum/(float)tagNumber;
-            result=(sum+temp->summary)/((float)tagNumber/2.0);
-            temp->summary=sum;
+            temp->k_element[k]=temp->k_element[k-1];
+            k--;
         }
+        sum=sum/(float)tagNumber;
+        result=(sum+temp->summary)/((float)tagNumber/2.0);
+        temp->summary=sum;
+    }
     return result;
 }
