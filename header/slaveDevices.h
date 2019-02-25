2546f12dd0e14008a0d3ac8d6ee3d9aac4bd6b48
adam <adam.herczig@ip-camp.com>
2019-02-25 21:40:11 +0100

    modified sensors and actuators constructos

--- header/slaveDevices.h
+++ header/slaveDevices.h
@@ -1,23 +1,41 @@
 #ifndef SLAVEDEVICES_H_INCLUDED
 #define SLAVEDEVICES_H_INCLUDED
-#include <iostream>
-#define DATALENGTH 8
 
+#include <iostream>
 #include <stdint.h>
+
+#define DATALENGTH 8
+#define POLYNOMIAL 0xd8
+#define BYTE 8
+#define WIDTH (BYTE * sizeof(uint16_t))
+#define TOPBIT (1 << (WIDTH-1))
+
+
+
+int addressCnt=0;
 
 enum sensorTypes
 {
-    temp_sensor,
+    temp_sensor=0,
     light_sensor,
     PIR_sensor,
-    humidity_sensor,
-    LCD
+    humidity_sensor
     
+};
+
+enum actuatorTypes
+{
+    transciever=0,
+    servoMotor,
+    motor,
+    LCD,
+    relay
+
 };
 
 struct packet
 {
-    int16_t *data;
+    int16_t data[BYTE];
     int8_t address;
     int32_t crc;
     int8_t length;
@@ -26,16 +44,21 @@ struct packet
 
 class devices
 {
-    packet Packet;
-    int types;
-    int heartBit;
-    int addressCnt=0;
-    void frameInit(int16_t *data,int8_t length);
-    void frameAssembly();
+    private:
+        
+        int heartBit;
+        void frameInit(int16_t *data,int8_t length);
+        void frameAssembly();
+        
+    protected:
+        packet Packet;
+        int types;
+        int address;
+        void setType(int type);
 
     public:
-    devices(int8_t address);
-    devices();
+        //devices(int8_t address,int type);
+        //devices(int type);
     
 
 };
@@ -45,13 +68,18 @@ class sensors : public devices
     uint8_t readFromLine();
     
     public:
+    sensors(int8_t address,int type);
+    sensors(int type);
     void read();
 };
 
 class actuators : public devices
 {
-
-    int sendOnLine();
+    int writeLine(int *data, int cmd, int fd);
+    public:
+    actuators(int8_t address,int type);
+    actuators(int type);
+    int send(int *data,int cmd);
 };
 /*Template pattern*/
 
--- src/slaveDevices.cpp
+++ src/slaveDevices.cpp
@@ -3,12 +3,119 @@
 
 using namespace std;
 
-devices::devices(int8_t address)
+/*devices::devices(int8_t address,int type)
 {
     devices.Packet.address=address;
+    setType(type);
+    catch(char message);
 }
 
-devices::devices()
+devices::devices(int type)
 {
+    devices.Packet.address=addressCnt++;
+}
+*/
+
+
+void devices::setType(int type)
+{
+        switch(type)
+        {
+            case temp_sensor:
+                sensors.types=type;
+                break;
+
+            case light_sensor:
+                sensors.types=type;
+                break;
+
+            case PIR_sensor:
+                sensors.types=type;
+                break;
+
+            case humidity_sensor:    
+                sensors.types=type;
+                break;
+            
+            default:
+                throw "Wrong typecode";
+                //break;
+
+        }
     
 }
+
+sensors::sensors(int8_t address,int type)
+{
+    sensors.Packet.address=address;
+    try
+    {
+        setType(type);
+    }
+    catch(char message)
+    {
+        //syslog
+        cout<<"bad type"<<endl;
+    }
+}
+
+sensors::sensors(int type)
+{
+    sensors.Packet.address=addressCnt++;
+    try
+    {
+        setType(type);
+    }
+    catch(char message)
+    {
+        //syslog
+        cout<<"bad type"<<endl;
+    }
+}
+
+actuators::actuators(int8_t address,int type)
+{
+    actuators.Packet.address=address;
+    try
+    {
+        setType(type);
+    }
+    catch(char message)
+    {
+        //syslog
+        cout<<"bad type"<<endl;
+    }
+}
+
+actuators::actuators(int type)
+{
+    actuators.Packet.address=addressCnt++;
+    try
+    {
+        setType(type);
+    }
+    catch(char message)
+    {
+        //syslog
+        cout<<"bad type"<<endl;
+    }
+}
+
+int actuators::writeLine(int *data, int cmd,int fd)
+{
+    uint8_t result;
+
+    return result
+}
+
+int actuators::send(int *data,int cmd,int fd)
+{
+    if(!data || sizeof(data)>BYTE || fd<0)
+        return 1;
+    int result=writeLine(data,cmd);
+    return result;
+
+}
+
+
+
