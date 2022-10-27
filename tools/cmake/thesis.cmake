c8d7b47097751ed7ac28668b7fa8ff355011e169
adam.herczig <adam.herczig@ip-camp.com>
2022-10-27 16:33:31 +0200

    clean the repo first of all

--- tools/cmake/thesis.cmake
+++
@@ -1,8 +0,0 @@
-cmake_minimum_required (VERSION 3.6)
-project (thesis)
-add_executable(thesis thesis.cxx)
-
-configure_file (
-	"${PROJECT_SOURCE_DIR}/*.c
-	"${PROJECT_HEADER_DIR}/*.c
-include_directories()
